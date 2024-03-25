bd4b7aa0c3fe8d7d48a5f8666a6cc7c760c2d02e
begin306 <162399945+begin306@users.noreply.github.com>
2024-03-26 00:05:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 document.getElementById('time').innerHTML = new Date().toLocaleString()
 
-function pink() {
-  document.body.style.backgroundColor = 'pink'
+function background() {
+  document.body.style.background = "url('img/cyber-security-1915626_1280.png')"
+  document.body.style.backgroundSize = 'cover'
+  document.body.style.backgroundPosition = 'center'
 }
 
-function lightblue() {
-  document.body.style.backgroundColor = 'lightblue'
-}
-
-function yellow() {
-  document.body.style.backgroundColor = 'yellow'
-}
-
-function linen() {
-  document.body.style.backgroundColor = 'linen'
+function reset() {
+  document.body.style.background = 'none'
 }
 
 function showhtml() {
-  document.getElemnetById('fig').src = 'img/html5.jpg'
-  document.getElemnetById('desc').innerHTML =
+  document.getElementById('fig').src = 'img/html5.jpg'
+  document.getElementById('desc').innerHTML =
     '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.'
 }
 
 function showcss() {
-  document.getElemnetById('fig').src = 'img/css3.jpg'
-  document.getElemnetById('desc').innerHTML =
-    '<b>css3</b>는HTML문서의 스타일을 지정하기 위한 언어이다.'
+  document.getElementById('fig').src = 'img/css3.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
 }
 
 function showjs() {
-  document.getElemnetById('fig').src = 'img/javascript.jpg'
-  document.getElemnetById('desc').innerHTML =
-    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행 할수 있게한다.'
+  document.getElementById('fig').src = 'img/javascript.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다. '
+}
+function github() {
+  document.getElementById('fig').src = 'img/pngimg.com - github_PNG19.png'
+  document.getElementById('desc').innerHTML =
+    '<b>github</b>는 Git을 기반으로 한 클라우드 기반의 코드 호스팅 플랫폼 이고 코드 공유 및 협업을 용이하게 해주며, 소스코드 리뷰, 이슈 트래킹, 프로젝트 관리 등 다양한 기능을 제공한다 '
 }
 
 function hide() {
-  document.getElemnetById('fig').src = ''
-  document.getElemnetById('desc').innerHTML = ''
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
 }
